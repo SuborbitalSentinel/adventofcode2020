24778690e3989b1155eb3adddbd66420b312cba8
Adam Miller <millera023@gmail.com>
2020-12-13 23:21:25 -0500

    day13: part 2 complete

--- day13/day13.go
+++ day13/day13.go
@@ -1,11 +1,14 @@
 package day13
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // Run day thirteen of advent
 func Run() {
 	fmt.Println("Day13 -- Part1: ", part1())
-	//fmt.Println("Day13 -- Part2: ", part2)
+	fmt.Println("Day13 -- Part2: ", part2())
 }
 
 // Hard coded everything for part 1 because it seemed silly to parse it
@@ -20,4 +23,30 @@ func part1() int {
 		}
 	}
 	panic("Bus not found")
+}
+
+type pair struct {
+	Offset int
+	ID     int
+}
+
+// Math trick problems are not fun to me...
+// Something something Chinese Remainder Theorm
+func part2() int {
+	busIds := [...]string{"29", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "37", "x", "x", "x", "x", "x", "467", "x", "x", "x", "x", "x", "x", "x", "23", "x", "x", "x", "x", "13", "x", "x", "x", "17", "x", "19", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "443", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "41"}
+	buses := []pair{}
+	for i, bus := range busIds {
+		if bus != "x" {
+			busID, _ := strconv.Atoi(bus)
+			buses = append(buses, pair{Offset: i, ID: busID})
+		}
+	}
+	time, step := 0, 1
+	for _, p := range buses {
+		for (time+p.Offset)%p.ID != 0 {
+			time += step
+		}
+		step *= p.ID
+	}
+	return time
 }
